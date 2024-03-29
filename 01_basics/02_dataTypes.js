834640d4968b3e7bf2f9780aca44ea64fdd08cbe
Ayushkumar302 <ayushxkumar2002@gmail.com>
2024-03-29 18:09:19 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use Strict"; // treat  all JS code as newer version
+// alert(3+3)
+
+/* data types in javascript 
+number: represents a numerical value. It can be an integer or floating point number.
+string:   represents text data. It is enclosed within either      single quotes (' ') or double quotes(" ").
+boolean: represents boolean values, which are true or false.
+null:  stand alone value | empty valuw |  represents the null value. Null is a special value that denotes 'no value' or 'nothing'.
+undefined: represents undefined values.
+symbol  : used to represent unique and immutable data.
+
+object   : represents collection of key-value pairs.     Each value is associated with a particular key.
+*/
+
+
+let name = "john"
+let age = 28
+let isLoggedIn = false
+let state = null // empty 
+
+// number => 2 to power 53 
+// bigint  : A bigInt is a numeric data type of JavaScript that allows us to work with arbitrarily large numbers.
+
+console.log(typeof name);
+console.log(typeof null);// object
+console.log(typeof  undefined); //undefined
